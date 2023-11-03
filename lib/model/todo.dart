ef77d71b99161dd317dec735ac5cdf03ce859073
santosh-5 <sidarthsantosh@gmail.com>
2023-11-03 15:06:54 +0530

    color changes

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -16,13 +16,6 @@ class ToDo {
       ToDo(id: '03', todoText: 'Check Emails', ),
       ToDo(id: '04', todoText: 'Team Meeting', ),
       ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
     ];
   }
 }
